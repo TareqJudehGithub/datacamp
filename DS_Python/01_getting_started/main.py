251abc309bed729a62decc45733a94ef3f9978ca
TareqJudehGithub <tareq.judeh@outlook.com>
2021-09-14 14:28:29 +0200

    First upload to GitHub.

---
+++ DS_Python/01_getting_started/main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+df = pd.read_csv("filename")
+plt.plot(df.letter_)
---
+++ DS_Python/01_getting_started/notes.py
@@ -0,0 +1,24 @@
+"""
+- Python is case-sensitive.
+- Importing Python modules
+ - Modules (sometimes called packages or libraries) help group together 
+   related sets of tools in Python.
+ - Numpy, a module for performing mathematical operations on lists of data.    
+ -  capitalization must exactly match the name of the module. 
+     import numpy as np  (import Numpy will return an error.)
+ - Strings
+  -  A string represents text. A string is surrounded by quotation marks 
+     (' or ") and can contain letters, numbers, and special characters.
+  -  It doesn't matter if you use single (') or double (") quotes, but it's 
+     important to be consistent throughout your code.
+ 
+ - Functions
+  - A function is an action, it turns one or more input, into an output.
+
+  def func_name(positional argument, positional argument, keyword argument) 
+   - positional arguments are seperated by commoas.
+   - keyword argument must come after the positional arguments.
+          label='argument_name'
+
+
+"""
